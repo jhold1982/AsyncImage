e6828fabde04463fc92593529d4239bdc00cb9c5
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-05-18 16:27:56 -0500

    Initial Commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Justin Hold on 5/18/23.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
